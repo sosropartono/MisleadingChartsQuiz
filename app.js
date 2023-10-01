8197a95472015b61854b485f815a89040f002bba
Yennhi <yennhi@yennhis-air.lan>
2023-09-30 23:56:21 -0700

    removed excess variables and created database

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 
 var con = mysql.createConnection({
   host: "localhost",
-  user: "user",
+  user: "root",
   password: "pw",
 });
 
@@ -16,59 +16,19 @@ con.connect(function (err) {
   const createDatabaseSQL =
     "CREATE DATABASE IF NOT EXISTS mydb CHARACTER SET utf8 COLLATE utf8_general_ci";
 
-  const createQuestionsTableSQL = `
-    CREATE TABLE IF NOT EXISTS questions (
-      questionNumber INT AUTO_INCREMENT PRIMARY KEY,
-      question VARCHAR(255) NOT NULL,
-      numberOfAnswers INT
-      )
-    `;
-
-  const createAnswersTableSQL = `
-    CREATE TABLE IF NOT EXISTS answers (
-      questionNumber INT,
-      answer VARCHAR(255),
-      )
-    `;
-
-  const insertDataSQL = `
-    INSERT INTO mydb.questions (questionNumber, question, numberOfAnswers) VALUES
-      (1, 'Is there drought in September?', 2),
-      (2, 'How many COVID hospitalizations in CA in Oct 2021?', 4),
-    `;
-
   con.query(createDatabaseSQL, (err) => {
     if (err) {
       console.error("Error creating database:", err);
     } else {
       console.log("Database created successfully");
-
-      // Create the table
-      con.query(createQuestionsTableSQL, (err) => {
-        if (err) {
-          console.error("Error creating table:", err);
-        } else {
-          console.log("Table created successfully");
-
-          // Insert data into the table
-          con.query(insertDataSQL, (err) => {
-            if (err) {
-              console.error("Error inserting data:", err);
-            } else {
-              console.log("Data inserted successfully");
-            }
-
-            // Close the MySQL connection when done
-            con.end((err) => {
-              if (err) {
-                console.error("Error closing connection:", err);
-              } else {
-                console.log("Connection closed");
-              }
-            });
-          });
-        }
-      });
     }
+  });
+
+  con.end((error) => {
+    if (error) {
+      console.error("Error closing MySQL connection:", error);
+      return;
+    }
+    console.log("MySQL connection closed.");
   });
 });
