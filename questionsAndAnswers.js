2d2f258432ded1e99f49ded64ccb7bf97aef0503
Yennhi <yennhi@yennhis-air.lan>
2023-10-03 08:20:17 -0700

    replaced login creds with dotenv variables

--- questionsAndAnswers.js
+++ questionsAndAnswers.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "mydb",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 con.connect(function (err) {
