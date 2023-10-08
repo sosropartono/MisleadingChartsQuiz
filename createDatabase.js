100f5ee1f06800311114929317264af9586614aa
Tnguyend1 <ukeynguyen@gmail.com>
2023-10-07 17:48:04 -0700

    quizsurvey and server modify to collect user data

--- createDatabase.js
+++ createDatabase.js
@@ -5,7 +5,7 @@ const con = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+ // database: process.env.DB_NAME,
 });
 
 con.connect(function (err) {
--- server.js
+++ server.js
@@ -1,56 +1,104 @@
-const express = require("express");
-const mysql = require("mysql");
-const path = require("path");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+const path = require('path');
 require("dotenv").config();
 
-var app = express();
-app.use(express.static(path.join(__dirname, "")));
+const app = express();
+const port = 3000;
 
-const con = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
-con.connect(function (err) {
-  if (err) {
-    console.error("Error connecting to MySQL:", err);
-    return;
-  }
-  console.log("Connected to MySQL server");
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+    console.log('Connected to MySQL database');
+});
 
-  //Define an API endpoint to fetch questions
-  app.get("/questions", (req, res) => {
-    const sql = "SELECT * FROM questions";
-    con.query(sql, (err, results) => {
-      if (err) {
-        console.error("Error fetching questions:", err);
-        res.status(500).json({ error: "Internal server error" });
-      } else {
-        var fetchedData = JSON.parse(JSON.stringify(results));
-        console.log(fetchedData);
-        res.send(fetchedData);
-      }
-    });
-  });
+let userCount = 0;
+let currentQuestionIndex = 0;
 
-  app.get("/answers", (req, res) => {
-    const sql = "SELECT * FROM answers";
-    con.query(sql, (err, results) => {
-      if (err) {
-        console.error("Error fetching answers:", err);
-        res.status(500).json({ error: "Internal server error" });
-      } else {
-        var fetchedData = JSON.parse(JSON.stringify(results));
-        console.log(fetchedData);
-        res.send(fetchedData);
-      }
-    });
-  });
+const surveyData = [
+    {
+        question: "Question1?",
+        chartImage: "chart1.png",
+        options: ["Yes", "No"],
+        correctAnswer: "Yes"
+    },
+    {
+        question: "Question2?",
+        chartImage: "chart2.png",
+        options: ["Yes", "No"],
+        correctAnswer: "Yes"
+    },
+    {
+        question: "Question3?",
+        chartImage: "chart3.png",
+        options: ["Yes", "No"],
+        correctAnswer: "Yes"
+    },
+    {
+        question: "Question4?",
+        chartImage: "chart4.png",
+        options: ["Yes", "No"],
+        correctAnswer: "Yes"
+    },
+    // more questions need to be added
+];
 
-  const port = 3000;
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/start-survey', (req, res) => {
+    userCount=0;
+    const userId = "User" + (++userCount);
+    currentQuestionIndex = 0;
+
+    res.json({ userId, question: surveyData[currentQuestionIndex] });
+});
+
+app.post('/submit-response', (req, res) => {
+    const { userId, userAnswer } = req.body;
+
+    const currentQuestion = surveyData[currentQuestionIndex];
+    const isCorrect = userAnswer === currentQuestion.correctAnswer;
+    console.log('Received response from user:', userId, 'with answer:', userAnswer);
+
+    connection.query(
+        'INSERT INTO survey_responses (user_id, question, user_answer, is_correct) VALUES (?, ?, ?, ?)',
+        [userId, currentQuestion.question, userAnswer, isCorrect],
+        (err, results) => {
+            if (err) {
+                console.error('Error inserting data:', err);
+                res.status(500).json({ error: 'Internal Server Error' });
+            } else {
+                console.log('Data inserted successfully');
+
+                // Move to the next question or end the survey
+                currentQuestionIndex++;
+                const nextQuestion = surveyData[currentQuestionIndex];
+
+                if (nextQuestion) {
+                    res.json({ status: 'success', message: 'User response received', question: nextQuestion });
+                } else {
+                    res.json({ status: 'success', message: 'Survey complete. Thank you for participating!' });
+                }
+            }
+        }
+    );
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
