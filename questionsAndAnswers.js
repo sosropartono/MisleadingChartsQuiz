50ae41129c63a6d456be421816db41111478f626
Yennhi <yennhi@yennhis-air.lan>
2023-11-08 23:53:45 -0800

    added real q&a to database

--- questionsAndAnswers.js
+++ questionsAndAnswers.js
@@ -18,6 +18,7 @@ con.connect(function (err) {
 
   const createTableSQL = `
     CREATE TABLE IF NOT EXISTS survey_questions (
+      question_id int PRIMARY KEY AUTO_INCREMENT,
       question_text VARCHAR(255) NOT NULL,
       chart_image VARCHAR(255) NOT NULL,
       options JSON NOT NULL,
@@ -46,7 +47,8 @@ con.connect(function (err) {
   const createResponsesTableSQL = `
     CREATE TABLE IF NOT EXISTS survey_responses (
       user_id VARCHAR(10) NOT NULL,
-      question VARCHAR(100),
+      question_id INT,
+      question VARCHAR(255),
       user_answer VARCHAR(10),
       is_correct VARCHAR(5) NOT NULL,
       timestamp VARCHAR(40)
@@ -57,7 +59,8 @@ con.connect(function (err) {
   CREATE TABLE IF NOT EXISTS master_table (
     user_id VARCHAR(10) NOT NULL,
     button_name VARCHAR(100),
-    question_text VARCHAR(50),
+    question_id INT,
+    question_text VARCHAR(255),
     user_answer VARCHAR(10),
     timestamp VARCHAR(40)
   )
@@ -65,12 +68,58 @@ con.connect(function (err) {
 
   //1 for correct answer and 0 for wrong answer
   const insertDataIntoSurveyQuestionsTable = `
-    INSERT INTO survey_questions (question_text, chart_image, options, correct_answer)
-    VALUES
-      ('Question1?', 'color_chart.png', '["yes1", "no"]', 'yes1'),
-      ('Question2?', 'color_chart.png', '["yes2", "no"]', 'no'),
-      ('Question3?', 'color_chart.png', '["Yes3", "No"]', 'Yes3')
-  `;
+    INSERT INTO survey_questions (question_text, chart_image, options, correct_answer) VALUES
+    ('According to the chart, there was a day (or days) where there were virtually no new COVID cases.', 'truncated_bar_chart.png', '["True", "False"]', 'False'),
+    ('Between January 7th and January 8th, the number of new cases more than doubled.', 'truncated_bar_chart_normal.png', '["True", "False"]', 'False'),
+    ('On January 16th, the number of new cases was about half that of the day before.', 'truncated_bar_chart.png', '["True", "False"]', 'False'),
+    ('On January 17th, the number of new cases decreased by more than half that of the day before.', 'truncated_bar_chart_normal.png', '["True", "False"]', 'False'),
+    ('From January 7 to January 17, the number of new cases generally declined.', 'truncated_bar_chart.png', '["True", "False"]', 'True'),
+    ('On January 12, there were about 250 new cases.', 'truncated_bar_chart_normal.png', '["True", "False"]', 'False'),
+    ('Between January 9 and January 10, there were less than 5 new COVID cases.', 'truncated_bar_chart.png', '["True", "False"]', 'False'),
+    ('On January 9, there were about 245,000 new COVID cases.', 'truncated_bar_chart_normal.png', '["True", "False"]', 'True'),
+    ('Between January 13th and January 14th, the number of new COVID cases declined by about 5,000.', 'truncated_bar_chart.png', '["True", "False"]', 'True'),
+    ('Between January 7th and January 17th, the highest daily count of new COVID cases surpassed 250,000.', 'truncated_bar_chart_normal.png', '["True", "False"]', 'True'),
+    ('Most of the fully vaccinated population became fully vaccinated in April.', '2d_pie_chart.png', '["True", "False"]', 'True'),
+    ('By May, about a quarter of the U.S. population was fully vaccinated.', 'cant find 2d-pie-chart-normal', '["True", "False"]', 'False'),
+    ('By August, at least half of the U.S. population was fully vaccinated.', 'cant find 2d-pie-chart-normal', '["True", "False"]', 'True'),
+    ('In June, half as many people became fully vaccinated as compared to the amount of people vaccinated in May.', 'cant find 2d-pie-chart-normal', '["True", "False"]', 'False'),
+    ('In March, more than double the amount of people became fully vaccinated as compared to the amount of people vaccinated in May.', '2d_pie_chart.png', '["True", "False"]', 'False'),
+    ('More people became fully vaccinated in the Summer (June, July, August) than in the Spring (March, April, May).', '2d_pie_chart.png', '["True", "False"]', 'True'),
+    ('More people became fully vaccinated in July than in February.', 'cant find 2d-pie-chart-normal', '["True", "False"]', 'True'),
+    ('More people became fully vaccinated in October than in January.', 'cant find 2d-pie-chart-normal', '["True", "False"]', 'False'),
+    ('More than half of the U.S. population was fully vaccinated by October.', '2d_pie_chart.png', '["True", "False"]', 'False'),
+    ('January was the month with the least amount of full vaccinations completed.', '2d_pie_chart.png', '["True", "False"]', 'True'),
+    ('There were around 3500 people in California hospitalized from COVID in the beginning of April 2021.', 'multiple-scale-line-chart.png', '["True", "False"]', 'False'),
+    ('There were around 2100 people in California hospitalized from COVID in the beginning of April 2021.', 'multiple-scale-line-chart-normal.png', '["True", "False"]', 'True'),
+    ('There were around 2000 people in California hospitalized from COVID in the beginning of April 2020.', 'multiple-scale-line-chart-normal.png', '["True", "False"]', 'True'),
+    ('There were around 1200 people in California hospitalized from COVID in the beginning of April 2020.', 'multiple-scale-line-chart.png', '["True", "False"]', 'False'),
+    ('There were around the same number of people in California hospitalized from COVID on April 11 in both 2020 and 2021.', 'multiple-scale-line-chart.png', '["True", "False"]', 'False'),
+    ('There were less people in California hospitalized from COVID on April 5 in 2020 than in 2021.', 'multiple-scale-line-chart-normal.png', '["True", "False"]', 'False'),
+    ('There were more people in California hospitalized from COVID on April 5 in 2020 than in 2021.', 'multiple-scale-line-chart.png', '["True", "False"]', 'True'),
+    ('There were less people in California hospitalized from COVID on April 5 in 2021 than in 2020.', 'multiple-scale-line-chart.png', '["True", "False"]', 'True'),
+    ('There were more people in California hospitalized from COVID on April 5 in 2021 than in 2020.', 'multiple-scale-line-chart-normal.png', '["True", "False"]', 'False'),
+    ('There were around 1700 people in California hospitalized from COVID in the end of April 2021.', 'multiple-scale-line-chart.png', '["True", "False"]', 'True'),
+    ('The day with the lowest number of COVID-related deaths in California in January was near the end of the month.', 'multiple-scale-line-chart-normal.png', '["True", "False"]', 'False'),
+    ('The day with the lowest number of COVID-related deaths in California in January was near the beginning of the month.', 'reversed-axis-bar-chart.png', '["True", "False"]', 'True'),
+    ('The day with the highest number of COVID-related deaths in California in January was in the first half of the month.', 'reversed-axis-bar-chart.png', '["True", "False"]', 'False'),
+    ('The day with the highest number of COVID-related deaths in California in January was in the second half of the month.', 'reversed-axis-bar-chart-normal.png', '["True", "False"]', 'True'),
+    ('There were more COVID-related deaths in California in the first half of the month than the second half.', 'reversed-axis-bar-chart.png', '["True", "False"]', 'False'),
+    ('There were more COVID-related deaths in California in the second half of the month than the first half.', 'reversed-axis-bar-chart-normal.png', '["True", "False"]', 'True'),
+    ('There were less COVID-related deaths in California in the first half of the month than the second half.', 'reversed-axis-bar-chart-normal.png', '["True", "False"]', 'True'),
+    ('There were less COVID-related deaths in California in the second half of the month than the first half.', 'reversed-axis-bar-chart.png', '["True", "False"]', 'False'),
+    ('The number of COVID-related deaths in California in January were trending upwards overall as time passed.', 'reversed-axis-bar-chart.png', '["True", "False"]', 'True'),
+    ('The number of COVID-related deaths in California in January were trending downwards overall as time passed.', 'reversed-axis-bar-chart-normal.png', '["True", "False"]', 'False'),
+    ('No vaccines had been administered at the beginning of January.', 'truncated-line-chart-normal.png', '["True", "False"]', 'False'),
+    ('Around 600000 total vaccines had been administered at the beginning of January.', 'truncated-line-chart.png', '["True", "False"]', 'True'),
+    ('No vaccines had been administered at the beginning of February.', 'truncated-line-chart-normal.png', '["True", "False"]', 'False'),
+    ('Around 4500000 total vaccines had been administered at the beginning of February.', 'truncated-line-chart.png', '["True", "False"]', 'True'),
+    ('Vaccines were administered in January.', 'truncated-line-chart-normal.png', '["True", "False"]', 'True'),
+    ('Vaccines were administered in February.', 'truncated-line-chart.png', '["True", "False"]', 'True'),
+    ('Vaccines began getting administered in March (according to the chart).', 'truncated-line-chart.png', '["True", "False"]', 'False'),
+    ('Vaccines had been administered before March (according to the chart).', 'truncated-line-chart-normal.png', '["True", "False"]', 'True'),
+    ('The total number of vaccines administered at the beginning of March was around 0.', 'truncated-line-chart.png', '["True", "False"]', 'False'),
+    ('The total number of vaccines administered at the beginning of March was around 100000.', 'truncated-line-chart-normal.png', '["True", "False"]', 'True')
+    `;
 
   // Drop the table if already exist
   con.query("DROP TABLE IF EXISTS survey_questions", (err) => {
