1c7e6e25fbcf67a4b237ff1192ca68fb17645158
Yennhi Dang <nhidangn@gmail.com>
2023-11-14 15:13:32 -0800

    deleted database from createDatabase.js

--- createDatabase.js
+++ createDatabase.js
@@ -4,8 +4,7 @@ require("dotenv").config();
 const con = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD
 });
 
 con.connect(function (err) {
