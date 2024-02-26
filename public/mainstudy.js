7f4d334a9377f783fd4dd37c177a2ab5d1d432a8
mievenfall <mngoc2603@gmail.com>
2024-02-26 02:11:23 -0800

    updated generateBatchD

--- public/mainstudy.js
+++ public/mainstudy.js
@@ -253,7 +253,7 @@ const generateBatchD = (current) => {
       new_elem.classList = "rec-desc"
       new_elem.style.paddingBottom = "20px"
       }
-      new_elem.textContent = data2DArray[currentQuestionIndex][3][z]
+      new_elem.textContent = val
       optionAList.appendChild(new_elem)
       optionAContainer.addEventListener("click", optionAClicked)
       
@@ -263,34 +263,34 @@ const generateBatchD = (current) => {
   // Column B
   let eng_esp_counter = 0
   // TODO: Change this so it includes more reccommendations, start index at 0
-  for(let j = 3; j < 6; j++) {
+  for(let j = 0; j < 6; j++) {
+    
     for(let p = 0; p < 2; p++ ){
       let new_elem = document.createElement("li")
+      let val
+
+      val = data2DArray[current][3][eng_esp_counter][j][p]
       
-      if (eng_esp_counter == 0){
-        eng_esp_counter = 1
-        val = data2DArray[current][3][eng_esp_counter][j][p]
-      }
-      else{
-        eng_esp_counter = 0
-        val = data2DArray[current][3][eng_esp_counter][j][p]
-      }
       if(p == 0){
         new_elem.style.fontWeight = "bold"
         new_elem.classList = "rec-title"
-
       }
       else{
         new_elem.classList = "rec-desc"
-      new_elem.style.paddingBottom = "20px"
-
-
+        new_elem.style.paddingBottom = "20px"
       }
+
       new_elem.textContent = val
       optionBList.appendChild(new_elem)
-      eng_esp_counter += 1
       optionBContainer.addEventListener("click", optionBClicked)
     }
+    if (eng_esp_counter == 0){
+      eng_esp_counter = 1
+    }
+    else{
+      eng_esp_counter = 0
+    }
+    // eng_esp_counter += 1
   }
 }
 
@@ -325,7 +325,6 @@ const generateBatchC = (current) => {
 
       val = data2DArray[current][3][eng_esp_counter][j][p]
       
-      console.log(val)
       if(p == 0){
         new_elem.style.fontWeight = "bold"
         new_elem.classList = "rec-title"
